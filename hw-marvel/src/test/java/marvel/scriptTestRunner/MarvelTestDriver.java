db416bfcff15842685db7e5756eb85bd7456499d
rayoh102 <rayoh7@gmail.com>
2021-02-18 20:07:03 -0800

    TestDriver

--- hw-marvel/src/test/java/marvel/scriptTestRunner/MarvelTestDriver.java
+++ hw-marvel/src/test/java/marvel/scriptTestRunner/MarvelTestDriver.java
@@ -11,8 +11,12 @@
 
 package marvel.scriptTestRunner;
 
-import java.io.Reader;
-import java.io.Writer;
+import graph.labelEdge;
+import graph.Graph;
+import marvel.MarvelPaths;
+
+import java.io.*;
+import java.util.*;
 
 /**
  * This class implements a testing driver which reads test scripts from
@@ -20,21 +24,324 @@ import java.io.Writer;
  */
 public class MarvelTestDriver {
 
+    // *********************************
+    // ***  Interactive Test Driver  ***
+    // *********************************
+
     public static void main(String[] args) {
         // You only need a main() method if you choose to implement
         // the 'interactive' test driver, as seen with GraphTestDriver's sample
         // code. You may also delete this method entirely if you don't want to
         // use the interactive test driver.
+
+        try {
+            if (args.length > 1) {
+                printUsage();
+                return;
+            }
+
+            MarvelTestDriver td;
+
+            if (args.length == 0) {
+                td = new MarvelTestDriver(new InputStreamReader(System.in), new OutputStreamWriter(System.out));
+                System.out.println("Running in interactive mode.");
+                System.out.println("Type a line in the script testing language to see the output.");
+            } else {
+                String fileName = args[0];
+                File tests = new File(fileName);
+
+                System.out.println("Reading from the provided file.");
+                System.out.println("Writing the output from running those tests to standard out.");
+                if (tests.exists() || tests.canRead()) {
+                    td = new MarvelTestDriver(new FileReader(tests), new OutputStreamWriter(System.out));
+                } else {
+                    System.err.println("Cannot read from " + tests.toString());
+                    printUsage();
+                    return;
+                }
+            }
+
+            td.runTests();
+
+        } catch (IOException e) {
+            System.err.println(e.toString());
+            e.printStackTrace(System.err);
+        }
+
+
     }
 
+    private static void printUsage() {
+        System.err.println("Usage:");
+        System.err.println("  Run the gradle 'build' task");
+        System.err.println("  Open a terminal at hw-graph/build/classes/java/test");
+        System.err.println("  To read from a file: java graph.scriptTestRunner.MarvelTestDriver <name of input script>");
+        System.err.println("  To read from standard in (interactive): java graph.scriptTestRunner.MarvelTestDriver");
+    }
+
+
+    // ***************************
+    // ***  JUnit Test Driver  ***
+    // ***************************
+
+    /**
+     * String -> Graph: maps the names of graphs to the actual graph
+     **/
+    private final Map<String, Graph<String, String>> graphs = new HashMap<>();
+    private final PrintWriter output;
+    private final BufferedReader input;
+
+    /**
+     * @spec.requires r != null && w != null
+     * @spec.effects Creates a new MarvelTestDriver which reads command from
+     * {@code r} and writes results to {@code w}
+     **/
     // Leave this constructor public
     public MarvelTestDriver(Reader r, Writer w) {
         // TODO: Implement this, reading commands from `r` and writing output to `w`.
         // See GraphTestDriver as an example.
+
+        input = new BufferedReader(r);
+        output = new PrintWriter(w);
+
     }
 
+    /**
+     * @throws IOException if the input or output sources encounter an IOException
+     * @spec.effects Executes the commands read from the input and writes results to the output
+     **/
     // Leave this method public
-    public void runTests() {
+    public void runTests() throws IOException {
         // TODO: Implement this.
+        String inputLine;
+        while ((inputLine = input.readLine()) != null) {
+            if ((inputLine.trim().length() == 0) ||
+                    (inputLine.charAt(0) == '#')) {
+                // echo blank and comment lines
+                output.println(inputLine);
+            } else {
+                // separate the input line on white space
+                StringTokenizer st = new StringTokenizer(inputLine);
+                if (st.hasMoreTokens()) {
+                    String command = st.nextToken();
+
+                    List<String> arguments = new ArrayList<String>();
+                    while (st.hasMoreTokens()) {
+                        arguments.add(st.nextToken());
+                    }
+
+                    executeCommand(command, arguments);
+                }
+            }
+            output.flush();
+        }
+
+    }
+
+    private void executeCommand(String command, List<String> arguments) {
+        try {
+            switch (command) {
+                case "CreateGraph":
+                    createGraph(arguments);
+                    break;
+                case "AddNode":
+                    addNode(arguments);
+                    break;
+                case "AddEdge":
+                    addEdge(arguments);
+                    break;
+                case "ListNodes":
+                    listNodes(arguments);
+                    break;
+                case "ListChildren":
+                    listChildren(arguments);
+                    break;
+                case "LoadGraph":
+                    loadGraph(arguments);
+                    break;
+                case "FindPath":
+                    findPath(arguments);
+                    break;
+                default:
+                    output.println("Unrecognized command: " + command);
+                    break;
+            }
+        } catch (Exception e) {
+            output.println("Exception: " + e.toString());
+        }
+    }
+
+    private void createGraph(List<String> arguments) {
+        if (arguments.size() != 1) {
+            throw new CommandException("Bad arguments to CreateGraph: " + arguments);
+        }
+
+        String graphName = arguments.get(0);
+        createGraph(graphName);
+    }
+
+    private void createGraph(String graphName) {
+        Graph<String, String> graph = new Graph<>();
+        graphs.put(graphName, graph);
+        output.println("created graph " + graphName);
+    }
+
+    private void addNode(List<String> arguments) {
+        if (arguments.size() != 2) {
+            throw new CommandException("Bad arguments to AddNode: " + arguments);
+        }
+
+        String graphName = arguments.get(0);
+        String nodeName = arguments.get(1);
+
+        addNode(graphName, nodeName);
+    }
+
+    private void addNode(String graphName, String nodeName) {
+        Graph<String, String> graph = graphs.get(graphName);
+        graph.addNode(nodeName);
+        output.println("added node " + nodeName + " to " + graphName);
+    }
+
+    private void addEdge(List<String> arguments) {
+        if (arguments.size() != 4) {
+            throw new CommandException("Bad arguments to AddEdge: " + arguments);
+        }
+
+        String graphName = arguments.get(0);
+        String parentName = arguments.get(1);
+        String childName = arguments.get(2);
+        String edgeLabel = arguments.get(3);
+
+        addEdge(graphName, parentName, childName, edgeLabel);
+    }
+
+    private void addEdge(String graphName, String parentName, String childName,
+                         String edgeLabel) {
+        Graph<String, String> graph = graphs.get(graphName);
+        graph.addEdge(parentName,childName,edgeLabel);
+        output.println("added edge " + edgeLabel + " from " + parentName + " to " + childName + " in " + graphName);
+    }
+
+    private void listNodes(List<String> arguments) {
+        if (arguments.size() != 1) {
+            throw new CommandException("Bad arguments to ListNodes: " + arguments);
+        }
+
+        String graphName = arguments.get(0);
+        listNodes(graphName);
+    }
+
+    private void listNodes(String graphName) {
+        Graph<String, String> graph = graphs.get(graphName);
+        List<String> listOfNodes = new ArrayList<>();
+        for(String node : graph.getNodes()) {
+            listOfNodes.add(node);
+        }
+        Collections.sort(listOfNodes);
+        output.print(graphName + " contains:");
+        for (String node: listOfNodes){
+            output.print(" " + node);
+        }
+        output.println();
+
+    }
+
+    private void listChildren(List<String> arguments) {
+        if (arguments.size() != 2) {
+            throw new CommandException("Bad arguments to ListChildren: " + arguments);
+        }
+
+        String graphName = arguments.get(0);
+        String parentName = arguments.get(1);
+        listChildren(graphName, parentName);
+    }
+
+    private void listChildren(String graphName, String parentName) {
+        Graph<String, String> graph = graphs.get(graphName);
+        List<labelEdge<String, String>> parent = graph.getEdges(parentName);
+        List<String> listOfEdges = new ArrayList<>();
+        for (labelEdge<String, String> edge: parent){
+            listOfEdges.add(edge.getDest() + "(" + edge.getLabel() + ")");
+        }
+        Collections.sort(listOfEdges);
+
+        output.print("the children of " + parentName + " in " + graphName + " are:" );
+        for (String string: listOfEdges) {
+            output.print(" " + string);
+        }
+        output.println();
+    }
+
+    private void loadGraph(List<String> arguments) {
+        if (arguments.size() != 2) {
+            throw new CommandException("Bad arguments to CreateGraph: " + arguments);
+        }
+
+        String graphName = arguments.get(0);
+        String fileName = arguments.get(1);
+        loadGraph(graphName, fileName);
+    }
+
+    private void loadGraph(String graphName, String fileName) {
+        Graph<String, String> graph = MarvelPaths.createGraph(fileName);
+        graphs.put(graphName, graph);
+        output.println("loaded graph " + graphName);
+    }
+
+
+    private void findPath(String graphName, String node_a, String node_b) {
+        Graph<String, String> graph = graphs.get(graphName);
+        node_a = node_a.replace('_', ' ');
+        node_b = node_b.replace('_', ' ');
+        if(!graph.containsNode(node_a)) {
+            output.println("unknown character " + node_a);
+        }
+        if(!graph.containsNode(node_b)) {
+            output.println("unknown character " + node_b);
+        }
+        if(graph.containsNode(node_a) && graph.containsNode(node_b)) {
+            MarvelPaths marvelPaths = new MarvelPaths();
+            List<labelEdge<String, String>> shortestPath = marvelPaths.searchPath(graph, node_a, node_b);
+            output.println("path from " + node_a + " to " + node_b + ":");
+            if(!node_a.equals(node_b)) {
+                if(shortestPath != null) {
+                    for(labelEdge<String, String> e : shortestPath) {
+                        output.println(e.getStart() + " to " + e.getDest() + " via " + e.getLabel());
+                    }
+                }
+                else {
+                    output.println("no path found");
+                }
+            }
+        }
+    }
+
+    private void findPath(List<String> arguments) {
+        if (arguments.size() != 3) {
+            throw new CommandException("Bad arguments to FindPath: " + arguments);
+        }
+
+        String graphName = arguments.get(0);
+        String node_a = arguments.get(1);
+        String node_b = arguments.get(2);
+
+        findPath(graphName, node_a, node_b);
+    }
+
+    /**
+     * This exception results when the input file cannot be parsed properly
+     **/
+    static class CommandException extends RuntimeException {
+
+        public CommandException() {
+            super();
+        }
+
+        public CommandException(String s) {
+            super(s);
+        }
+
+        public static final long serialVersionUID = 3495;
     }
 }
